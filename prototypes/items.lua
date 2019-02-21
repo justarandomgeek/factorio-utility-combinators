dc1dc0b2bf3511ff1914ba97d4af968fc2477c6e
Thomas Deal <justarandomgeek@gmail.com>
2019-02-20 20:55:44 -0500

    Update for 0.17

--- prototypes/items.lua
+++ prototypes/items.lua
@@ -3,7 +3,7 @@ data:extend{
     type = "item",
     name = "player-combinator",
     icon = "__base__/graphics/icons/constant-combinator.png",
-    flags = {"goes-to-quickbar"},
+    icon_size = 32,
     subgroup = "circuit-network",
     place_result="player-combinator",
     order = "b[combinators]-d[player-combinator]",
