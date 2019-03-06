226ea9a67b17a10718486fd4ddb5c00c5a3ecb4b
Thomas Deal <justarandomgeek@gmail.com>
2019-03-06 15:22:51 -0500

    Control and merge cleanup

--- control.lua
+++ control.lua
@@ -1,4 +1,13 @@
-local function UpdateBonuses()
+function get_signal_from_set(signal,set)
+  for _,sig in pairs(set) do
+    if sig.signal.type == signal.type and sig.signal.name == signal.name then
+      return sig.count
+    end
+  end
+  return nil
+end
+
+function UpdateBonuses()
   for forcename,force in pairs(game.forces) do
     global.bonusframe[forcename] = {
       --{index=1,count=0,signal={name="signal-grey",type="virtual"}}
@@ -14,148 +23,7 @@ local function UpdateBonuses()
   end
 end
 
-local function onChange(event)
-  UpdateBonuses()
-  for forcename,controls in pairs(global.controls) do
-    for _,control in pairs(controls) do
-      if control.valid then
-        control.parameters={enabled=true,parameters=global.bonusframe[forcename] or {}}
-      else
-        table.remove(global.controls[forcename],_)
-      end
-    end
-  end
-end
-
-local function onBuilt(event)
-  local entity=event.created_entity
-  if entity.name == "bonus-combinator" then
-    entity.operable = false
-    global.controls[entity.force.name] = global.controls[entity.force.name] or {}
-    local control = entity.get_or_create_control_behavior()
-    table.insert(global.controls[entity.force.name],control)
-    control.parameters={enabled=true,parameters=global.bonusframe[entity.force.name] or {}}
-  elseif entity.name == "location-combinator" then
-    local control = entity.get_or_create_control_behavior()
-    control.enabled=true
-    control.parameters={parameters={
-      {index=1,count=math.floor(entity.position.x),signal={name="signal-X",type="virtual"}},
-      {index=2,count=math.floor(entity.position.y),signal={name="signal-Y",type="virtual"}},
-      {index=3,count=entity.surface.index,signal={name="signal-Z",type="virtual"}}
-    }}
-    entity.operable=false
-  elseif entity.name == "player-combinator" then
-    global.controls = global.controls or {}
-    local control = entity.get_or_create_control_behavior()
-    table.insert(global.controls,control)
-  elseif entity.name == "research-combinator" then
-    entity.operable = false
-    global.controls[entity.force.name] = global.controls[entity.force.name] or {}
-    local control = entity.get_or_create_control_behavior()
-    table.insert(global.controls[entity.force.name],control)
-  end
-end
-
-local function onInit()
-  -- bonus combinator
-  global.controls = {}
-  global.bonusframe = {}
-  UpdateBonuses()
-
-  -- player combinator
-  global.playerframes = {}
-  for i,p in pairs(game.players) do
-    global.playerframes[i]=playerFrame(p)
-  end
-
-  -- research combinator
-  global.controls = {}
-  UpdateResearch()
-end
-
-local function get_signal_value(control,signal)
-	local redval,greenval=0,0
-
-	local rednetwork = control.get_circuit_network(defines.wire_type.red)
-	if rednetwork then
-	  redval = rednetwork.get_signal(signal)
-	end
-
-	local greennetwork = control.get_circuit_network(defines.wire_type.green)
-	if greennetwork then
-	  greenval = greennetwork.get_signal(signal)
-	end
-	return(redval + greenval)
-end
-
-local function playerFrame(player)
-  return remote.call('signalstrings','string_to_signals',player.name,{
-    {index=1,count=player.connected and 1 or 0 ,signal={name="signal-green",type="virtual"}},
-    {index=2,count=player.admin and 1 or 0 ,signal={name="signal-red",type="virtual"}},
-  })
-end
-
-
-local function onPlayerCreated(event)
-  local i = event.player_index
-  local p = game.players[i]
-  global.playerframes[i]=playerFrame(p)
-end
-
-local function onTick()
-  --player
-  if game.tick%300==0 then
-    for i,p in pairs(game.players) do
-      global.playerframes[i]=playerFrame(p)
-    end
-
-    global.globalframe={
-      {index=1,count=#game.connected_players,signal={name="signal-green",type="virtual"}},
-      {index=2,count=#game.players,signal={name="signal-blue",type="virtual"}},
-    }
-  end
-
-  if global.controls then
-    if not global.controls[global.nextcontrol] then
-      global.nextcontrol = nil
-    end
-    local control = {}
-    global.nextcontrol,control = next(global.controls,global.nextcontrol)
-    if control then
-      if control.valid then
-        local req = get_signal_value(control,{name="signal-grey",type="virtual"})
-        if req == 0 then
-          control.parameters={enabled=true,parameters=global.globalframe}
-        else
-          control.parameters={enabled=true,parameters=global.playerframes[req] or {}}
-        end
-      else
-        table.remove(global.controls,global.nextcontrol)
-        global.nextcontrol=nil
-      end
-    end
-  end
-
-  --research
-  if game.tick % 60 == 0 then
-
-    UpdateResearch()
-
-    for forcename,controls in pairs(global.controls) do
-      --global.researchframe[forcename][1].count=math.floor(game.forces[forcename].research_progress * 100)
-
-      for _,control in pairs(controls) do
-        if control.valid then
-          control.parameters={enabled=true,parameters=global.researchframe[forcename] or {}}
-        else
-          table.remove(global.controls[forcename],_)
-        end
-      end
-    end
-  end
-end
-
-local function UpdateResearch(event)
+function UpdateResearch()
   local newframes = {}
   for forcename,force in pairs(game.forces) do
 
@@ -178,7 +46,7 @@ local function UpdateResearch(event)
         }
       end
 
-      if remote.interfaces['signalstrings'] and remote.interfaces['signalstrings']['string_to_signals'] then
+      if remote.interfaces['signalstrings'] then
         newframes[forcename] = remote.call('signalstrings','string_to_signals',force.current_research.name, extras)
       else
         newframes[forcename] = extras
@@ -193,17 +61,175 @@ local function UpdateResearch(event)
   global.researchframe = newframes
 end
 
-script.on_event(defines.events.on_research_started, UpdateResearch)
-script.on_event(defines.events.on_research_finished, UpdateResearch)
-script.on_event(defines.events.on_force_created, UpdateResearch)
-script.on_event(defines.events.on_forces_merging, UpdateResearch)
+function playerFrame(player)
+  local extras = {
+    {index=1,count=player.connected and 1 or 0 ,signal={name="signal-green",type="virtual"}},
+    {index=2,count=player.admin and 1 or 0 ,signal={name="signal-red",type="virtual"}},
+  }
+  if remote.interfaces['signalstrings'] then
+    return remote.call('signalstrings','string_to_signals',player.name, extras)
+  else
+    return extras
+  end
+end
 
+function onForceResearchChange(event)
+  UpdateBonuses()
+  UpdateResearch()
 
-script.on_event(defines.events.on_research_finished, onChange)
-script.on_event(defines.events.on_force_created, onChange)
-script.on_event(defines.events.on_forces_merging, onChange)
+  for n,rcc in pairs(global.researchcc) do
+    if rcc.entity.valid and rcc.control.valid then
+      rcc.control.parameters={enabled=true,parameters=global.researchframe[rcc.entity.force.name] or {}}
+    else
+      global.researchcc[n] = nil
+    end
+  end
+  for n,bcc in pairs(global.bonuscc) do
+    if bcc.entity.valid and bcc.control.valid then
+      bcc.control.parameters={enabled=true,parameters=global.bonusframe[bcc.entity.force.name] or {}}
+    else
+      global.bonuscc[n] = nil
+    end
+  end
+end
+
+function onBuilt(event)
+  local entity=event.created_entity
+  if entity.name == "bonus-combinator" then
+    entity.operable = false
+
+    local control = entity.get_or_create_control_behavior()
+    control.parameters={enabled=true,parameters=global.bonusframe[entity.force.name] or {}}
+    global.bonuscc[entity.unit_number] = {entity=entity,control=control}
+
+  elseif entity.name == "location-combinator" then
+    local control = entity.get_or_create_control_behavior()
+    control.enabled=true
+    control.parameters={parameters={
+      {index=1,count=math.floor(entity.position.x),signal={name="signal-X",type="virtual"}},
+      {index=2,count=math.floor(entity.position.y),signal={name="signal-Y",type="virtual"}},
+      {index=3,count=entity.surface.index,signal={name="signal-Z",type="virtual"}}
+    }}
+    entity.operable=false
+  elseif entity.name == "player-combinator" then
+
+    local control = entity.get_or_create_control_behavior()
+    global.playercc[entity.unit_number] = {entity=entity,control=control}
+  elseif entity.name == "research-combinator" then
+    entity.operable = false
+    local control = entity.get_or_create_control_behavior()
+    global.researchcc[entity.unit_number] = {entity=entity,control=control}
+    control.parameters={enabled=true,parameters=global.researchframe[entity.force.name] or {}}
+  end
+end
+
+local function onInit()
+  global = {
+    bonuscc = {
+      --[unit_number] = {entity, control},
+    },
+    bonusframe = {
+      --[force.name] = ccdata,
+    },
+
+    playercc = {
+      --[unit_number] = {entity, control},
+    },
+    playerframes = {
+      --[player.index] = ccdata,
+    },
+
+    researchcc = {
+      --[unit_number] = {entity, control},
+    },
+    researchframe = {
+      --[force.name] = ccdata,
+    },
+
+  }
+
+  -- bonus combinator
+  UpdateBonuses()
+
+  -- player combinator
+  for i,p in pairs(game.players) do
+    global.playerframes[i]=playerFrame(p)
+  end
+  global.globalplayerframe={
+    {index=1,count=#game.connected_players,signal={name="signal-green",type="virtual"}},
+    {index=2,count=#game.players,signal={name="signal-blue",type="virtual"}},
+  }
+
+  -- research combinator
+  UpdateResearch()
+
+  -- index existing combinators (init and config changed to capture from deprecated mods as well)
+  -- and re-index the world
+  for _,surf in pairs(game.surfaces) do
+    -- re-index all nixies. non-nixie lamps will be ignored by onPlaceEntity
+    for _,ent in pairs(
+      surf.find_entities_filtered{name =
+      {"bonus-combinator", "location-combinator", "player-combinator", "research-combinator",}}
+    ) do
+      onBuilt({created_entity=ent})
+    end
+  end
+end
+
+function onConfigChanged(data)
+  if data.mod_changes and data.mod_changes["utility-combinators"] then
+   --If my data has changed, rebuild all my tables.
+   -- OnInit has to rebuild thigns anyway to catch deprecated single-combinator mods
+   OnInit()
+  end
+end
+
+function onPlayerChanged(event)
+  local i = event.player_index
+  local p = game.players[i]
+  global.playerframes[i]=playerFrame(p)
+
+  global.globalplayerframe={
+    {index=1,count=#game.connected_players,signal={name="signal-green",type="virtual"}},
+    {index=2,count=#game.players,signal={name="signal-blue",type="virtual"}},
+  }
+end
+
+function onTick()
+  --player
+  for n,pcc in pairs(global.playercc) do
+    if pcc.entity.valid and pcc.control.valid then
+
+      local signals = pcc.entity.get_merged_signals() or {}
+      local req = get_signal_from_set({name="signal-grey",type="virtual"},signals) or 0
+      if req == 0 then
+        pcc.control.parameters={enabled=true,parameters=global.globalplayerframe}
+      else
+        pcc.control.parameters={enabled=true,parameters=global.playerframes[req] or {}}
+      end
+    else
+      global.playercc[n] = nil
+    end
+  end
+
+  --research
+  --TODO: move to an on_nth_tick
+  if game.tick % 60 == 0 then
+
+    UpdateResearch()
+
+    for n,rcc in pairs(global.researchcc) do
+      if rcc.entity.valid and rcc.control.valid then
+        rcc.control.parameters={enabled=true,parameters=global.researchframe[rcc.entity.force.name] or {}}
+      else
+        global.researchcc[n] = nil
+      end
+    end
+  end
+end
+
+script.on_event({defines.events.on_research_started, defines.events.on_research_finished, defines.events.on_force_created,defines.events.on_forces_merging}, onForceResearchChange)
 script.on_event(defines.events.on_tick, onTick)
-script.on_event(defines.events.on_built_entity, onBuilt)
-script.on_event(defines.events.on_robot_built_entity, onBuilt)
-script.on_event(defines.events.on_player_created, onPlayerCreated)
+script.on_event({defines.events.on_built_entity, defines.events.on_robot_built_entity}, onBuilt)
+script.on_event({defines.events.on_player_created, defines.events.on_player_joined_game, defines.events.on_player_left_game, defines.events.on_player_promoted, defines.events.on_player_demoted}, onPlayerChanged)
 script.on_init(onInit)
